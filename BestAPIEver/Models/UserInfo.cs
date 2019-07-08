391d90380807b5d51a6a423ad4da502d62cf8e61
ALMLAMBO <aleksm007148@gmail.com>
2019-07-08 14:32:14 +0300

    Add user info model

---
+++ BestAPIEver/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestAPIEver.Models {
+	public class UserInfo {
+		public int Age { get; set; }
+		public string FullName { get; set; }
+	}
+}
